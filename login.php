5cdbd06bd5424b9a89118f4e400de74035fc389e
Selin Kaya <selinkaya99@gmail.com>
2021-05-14 19:58:06 +0300

    Updated login and logout screens

---
+++ login.php
@@ -0,0 +1,45 @@
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Track Base - Log in</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+
+    <div class="grid-container">
+
+            <div class="left-side">
+                <img src = "assets/Track your favorite.svg"></img>
+            </div>
+
+            <div class="right-side">
+                <div class="page-title">
+                    Log In
+                </div>
+
+                <div class="email-address">
+                    Email Address
+                    <form method="post">
+                        <input type="text" name="email"/>
+                    </form>
+                </div>
+
+                <div class="password">
+                    Password
+                    <form method="post">
+                        <input type="password" name="password"/>
+                    </form>
+                </div>
+
+                <div class="link-to">
+                    New user? Sign up <a href="sign_up.php">here</a>.
+                </div>
+            </div>
+
+    </div>
+
+</body>
+
+</html>
---
+++ sign_up.php
@@ -0,0 +1,45 @@
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Track Base - Sign Up</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+
+    <div class="grid-container">
+
+            <div class="left-side">
+                <img src = "assets/Track your favorite.svg"></img>
+            </div>
+
+            <div class="right-side">
+                <div class="page-title">
+                    Sign Up
+                </div>
+
+                <div class="email-address">
+                    Email Address
+                    <form method="post">
+                        <input type="text" name="email"/>
+                    </form>
+                </div>
+
+                <div class="password">
+                    Password
+                    <form method="post">
+                        <input type="password" name="password"/>
+                    </form>
+                </div>
+
+                <div class="link-to">
+                    Already a user? Log in <a href="login.php">here</a>.
+                </div>
+            </div>
+
+    </div>
+
+</body>
+
+</html>
